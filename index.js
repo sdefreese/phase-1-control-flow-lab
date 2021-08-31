5e8f7a229bd4aad21d43a9bf7a9f42ed45d357d3
sdefreese <samanthadefreese1@gmail.com>
2021-08-30 18:09:39 -0700

    commit tree 7ffc039f643cbfeedecee4dc1d2750e170eb4811

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rigSize) {
+  if (rigSize <= 400) { 
+    return "This one is on me!"
+  } 
+  else if (rigSize <=2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (rigSize >2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else if (city != 'NYC') {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    case 'thanks for everything' :
+      return "Bye."
+      break;
+  }
 }
